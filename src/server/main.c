7e0bc36966aab3211c6297d164fcc6b3077d3d73
Felipe <feliog1234@gmail.com>
2025-08-14 20:57:33 -0600

    start epoll

--- src/server/main.c
+++ src/server/main.c
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
+#include <poll.h>
+
 #define backlog 20
 
 
@@ -18,8 +20,8 @@ int main(int argc,char *argv[]){
   const char * PORT = "4000";
   int sock,new_sock;
   const char * msg = "Mensaje para cliente";
+  struct poll pollfds[];
   memset(&hints,0,sizeof(hints));
-  
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_protocol = 0;
@@ -33,63 +35,59 @@ int main(int argc,char *argv[]){
   }
 
   int sock1 = socket(res ->ai_family,res->ai_socktype,res->ai_protocol);
- 
-  int res_bind = bind(sock1,res->ai_addr,res->ai_addrlen);
-  assert(res_bind!=-1);
+  int res_bind = bind(sock1,res->ai_addr,res->ai_addrlen); assert(res_bind!=-1);
 
-  int res_lis =listen(sock1,backlog);
+  int res_lis = listen(sock1,backlog);
+  
 
-  if (res_lis==0){
-    int listening  = 1;
+  if (res_lis == 0) {
+    int listening = 1;
     char msg_buffer[200];
     addr_size = sizeof(their_addr);
-    printf("Escuchando clientes en el puerto %s\n",PORT);
-    while(listening){
-   
-        //Accept espera el proximo mensaje que llegue al puerto y lo acepta
+    printf("Escuchando clientes en el puerto %s\n", PORT);
+    int active_sockets[30];
+    
+    int socket_index = 0;
+
+    while (listening) {
+        // Accept espera el proximo mensaje que llegue al puerto y lo acepta
         int new_sock = accept(sock1, (struct sockaddr *)&their_addr, &addr_size);
-        assert(new_sock!=-1);
+        assert(new_sock != -1);
+        printf("nuevo cliente conectado con el socket %d\n", new_sock);
         
+        active_sockets[socket_index] = new_sock;
+    
         int res_recv = recv(new_sock, msg_buffer, sizeof(msg_buffer), 0);
-        //TODO: revisar el numero del socket descriptor si un usuario cierra el proceso cliente una y otra vez
-        if (res_recv>0){
-            
+        assert(res_recv != -1);
+
+        if (res_recv > 0) {
             msg_buffer[res_recv] = '\0';
-            
-
-            char terminado[] = "exit";
-            
-            printf("el cliente dice : %s\n",msg_buffer);
-            
-            if (strcmp(msg_buffer,terminado)==0){
-            
-                printf("El usuario desea terminar la conneccion\n");
-                close(new_sock);
-                exit(1);
-            }
-            
+            printf("el cliente con el id %d dice %s\n", new_sock, msg_buffer);
         }
-        assert(res_recv!=-1);
-              
-        int res = send(new_sock,msg,strlen(msg),0);
+
+        char msg[200];
+        sprintf(msg, "Mensaje para el socket %d", new_sock);
         
-        if (res>0){
-            printf("Mensaje enviado\n");
 
+        int res = send(new_sock, msg, strlen(msg), 0);
+        assert(res != -1);
+
+        if (res > 0) {
+            printf("Mensaje enviado\n");
         }
 
-        assert(res !=-1);
-
+        // Si quieres cerrar el socket después de enviar el mensaje
+        // close(new_sock);
     }
-    
-     
-  }else{
-    fprintf(stderr,"Listening failed");
-    return -1;
-  }
-  
-  freeaddrinfo(res);
 
-    
-  return 0;
+} else {
+    fprintf(stderr, "Listening failed");
+    return -1;
 }
+
+freeaddrinfo(res);
+close(new_sock);
+
+return 0;
+}
+

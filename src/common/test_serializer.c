5b534e0413b87c22b72e6111e146f686a3f3ca2b
Felipe <feliog1234@gmail.com>
2025-09-07 20:51:21 -0600

    change struct packet init to keep same practice

--- src/common/test_serializer.c
+++ src/common/test_serializer.c
@@ -6,11 +6,8 @@ char buffer[500];
 fgets(buffer,sizeof(buffer),stdin);
 buffer[strlen(buffer)-1] = 0;
 ClientMessage m = {.message_buffer = buffer,.payload_len = strlen(buffer)};
+struct packet pa = {.flags = 0x01, .struct_pointer = &m};
 
-struct packet pa;
-
-pa.flags = 0x01;
-pa.struct_pointer = &m;
 char *final_buffer;
 serialize_message(pa,final_buffer);
 

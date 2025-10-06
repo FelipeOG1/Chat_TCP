29a47747b01162ded528f6a8472779143c7d95e7
FelipeOG1 <feliog1234@gmail.com>
2025-10-05 18:51:09 -0600

    use constant instead of name_len +1

--- src/server/rooms.c
+++ src/server/rooms.c
@@ -9,7 +9,7 @@ static int room_id_counter = 0;
 static void init_room(Room *room,AddRoom *add_room_msg,int client_sockfd){
   room->sockets_index = 0;
   room->room_name_len = strlen(add_room_msg->room_name);
-  strlcpy(room->room_name,add_room_msg->room_name,room->room_name_len + 1);//+1 pos add '\0'
+  strlcpy(room->room_name,add_room_msg->room_name,MAX_ROOM_NAME_LEN);//+1 pos add '\0'
   room->clients_sockets[room->sockets_index] = client_sockfd;//add creator of room socket into room sockets
   room->room_id = room_id_counter++;//room_id = room_id_counter and then increment by 1
 }
@@ -26,7 +26,7 @@ void add_room(Rooms *rooms ,AddRoom * add_room,int sockfd){
   init_room(&room,add_room,sockfd);
   rooms->all_rooms[room.room_id] = room;
   size_t current_room_name_len = room.room_name_len;
-  strlcpy(rooms->room_names[rooms->n_rooms],room.room_name,current_room_name_len + 1);
+  strlcpy(rooms->room_names[rooms->n_rooms],room.room_name,MAX_ROOM_NAME_LEN);
   rooms->n_rooms+=1;
   rooms->room_names_len+=current_room_name_len;
 }

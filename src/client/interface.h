01772f28f2f394b5fe06aaed1fcf3a2abdd4c4b4
FelipeOG1 <feliog1234@gmail.com>
2025-10-13 19:27:16 -0600

    [UI]refactor render menu

--- src/client/interface.h
+++ src/client/interface.h
@@ -1,6 +1,7 @@
 #include "../common/packet.h"
 
 #pragma once
+void render_menu_and_get_response(char *options[],int n_options,char res_buffer[],size_t buffer_size);
 void render_chat_room_window(int sockfd);
 void render_create_room_window(int sockfd);
 void render_join_room_window(int sockfd);
